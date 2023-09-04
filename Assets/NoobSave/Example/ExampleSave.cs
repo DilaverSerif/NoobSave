e9c0043d93f0a012bc7fc1ac1a139c6f8c018c06
DilaverSerif <dilaver.serif@outlook.com>
2023-09-04 09:52:48 +0300

    Example Save Changed

--- Assets/NoobSave/Example/ExampleSave.cs
+++ Assets/NoobSave/Example/ExampleSave.cs
@@ -1,40 +1,42 @@
 using System;
-using NoobSave;
 using NoobSave.Interfaces;
 using UnityEngine;
 
-public class ExampleSave : MonoBehaviour,INoobSaveable
+namespace NoobSave.Example
 {
-    [Serializable]
-    public struct GameObjectSave
+    public class ExampleSave : MonoBehaviour,INoobSaveable
     {
-        public Vector3 position;
-        public Vector3 rotation;
-        
-        public GameObjectSave(Vector3 position, Vector3 rotation)
+        [Serializable]
+        public struct GameObjectSave
         {
-            this.position = position;
-            this.rotation = rotation;
+            public Vector3 position;
+            public Vector3 rotation;
+        
+            public GameObjectSave(Vector3 position, Vector3 rotation)
+            {
+                this.position = position;
+                this.rotation = rotation;
+            }
         }
-    }
 
-    private void Start()
-    {
-        NoobSaveMain.Load();
-        Debug.Log( NoobSaveMain.GetSave<string>("NormalKeyTest"));
-    }
+        private void Start()
+        {
+            NoobSaveMain.Load();
+            Debug.Log( NoobSaveMain.GetSave<string>("NormalKeyTest"));
+        }
 
-    public void Save()
-    {
-        var gbSave = new GameObjectSave(transform.position,transform.rotation.eulerAngles);
-        NoobSaveMain.AddSave("ExampleSave",gbSave);
-        NoobSaveMain.AddSave("InterfaceKeyTest","Interface is working!");
-        NoobSaveMain.AddSave("NormalKeyTest","Normal key is working!");
-    }
+        public void Save()
+        {
+            var gbSave = new GameObjectSave(transform.position,transform.rotation.eulerAngles);
+            NoobSaveMain.AddSave("ExampleSave",gbSave);
+            NoobSaveMain.AddSave("InterfaceKeyTest","Interface is working!");
+            NoobSaveMain.AddSave("NormalKeyTest","Normal key is working!");
+        }
 
-    public void Load(ref SaveData saveData)
-    {
-        Debug.Log(saveData.GetSave<string>("InterfaceKeyTest"));
-    }
+        public void Load(ref SaveData saveData)
+        {
+            Debug.Log(saveData.GetSave<string>("InterfaceKeyTest"));
+        }
     
+    }
 }
